f3eae1f9fb26dacbaa3b9ec8af7c31694283f7ef
spectrecoder <pong96111kind@gmail.com>
2024-05-05 23:41:10 +0900

    faq update

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -54,7 +54,7 @@ if ! command -v node >/dev/null || ! command -v git >/dev/null || ! command -v y
 fi
 
 # Clone the repository and install dependencies
-git clone https://github.com/hercles321/Commune-MultiChatGPT
+git clone https://github.com/hercles321/com-multi-chatgpt
 cd ChatGPT-Next-Web
 yarn install
 
